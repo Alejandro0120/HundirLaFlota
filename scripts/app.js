2b8700df09375b14862588c8ded7561d4d699582
Alejandro0120 <73612152+Alejandro0120@users.noreply.github.com>
2022-02-15 20:42:55 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,20 @@
 document.querySelector('#enviar').addEventListener('click',imprimirTablero);
 
+class Barco {
+    constructor(tamallo, direccion){
+        this.tamallo = tamallo;
+        this.direccion = direccion;
+    }
+}
+
+
+
+
+
+
+
+
+
 
 function disparar(e){
     if(e.target.className != 'tablero'){
@@ -8,6 +23,38 @@ function disparar(e){
         console.log(e.target.className);
     }
     
+}
+
+function crearFormularioBarco(){
+    let crearBarco = document.querySelector('#crearBarco');
+    let fragmentBarco = document.createDocumentFragment();
+    let h2 = document.createElement('h1');
+    h2.textContent = 'Creación de Barcos';
+    
+    let form = document.createElement('form');
+    let div1 = document.createElement('div');
+    let label1 = document.createElement('label');
+    label1.textContent = 'Tamaño del Barco: ';
+
+    let inputNum=document.createElement('input')
+    inputNum.type = 'number';
+    inputNum.id = 'tamallo';
+    inputNum.min = 1;
+    inputNum.max = 4;
+    inputNum.value = 1;
+
+    let div2 = document.createElement('div');
+    let label2 = document.createElement('label');
+    label2.textContent = 'Direccion del barco: ';
+
+    let inputRadio=document.createElement('input');
+    inputRadio.value 
+
+    div1.append(label1, inputNum);
+    form.append(div1);
+    fragmentBarco.append(h2,form);
+    crearBarco.appendChild(fragmentBarco);
+
 }
 
 
@@ -48,6 +95,8 @@ function imprimirTablero(){
     }
     table.appendChild(fragment)
     tablero.appendChild(table)
+
+    crearFormularioBarco();
 
     document.querySelector('table').addEventListener('click',disparar);
 
